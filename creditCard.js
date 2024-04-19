ffcea227dd8f54236ab3f7bca92a2d0fcce63a32
H-Yurii <134538109+H-Yurii@users.noreply.github.com>
2024-04-19 23:28:28 +0200

    Second change

--- creditCard.js
+++ creditCard.js
@@ -1,36 +1,28 @@
-let cardName = document.querySelector('.cardName')
-let textCard = document.querySelector('.textCard')
-let costumerName = document.querySelector('.costumerName')
-let textName = document.querySelector('.textName')
-let iBAN = document.querySelector('.iBAN')
-let textIban = document.querySelector('.textIban')
-let bLZ = document.querySelector('.bLZ')
-let textBlz = document.querySelector('.textBlz')
-let kontoNr = document.querySelector('.kontoNr')
-let textNr = document.querySelector('.textNr')
-let date = document.querySelector('.date')
-let textDate = document.querySelector('.textDate')
-cardName.addEventListener('input', function () {
-    let cardNameValue = cardName.value
-    textCard.textContent = cardNameValue
+let cardNumber = document.querySelector('.cardNumber')
+let customerName = document.querySelector('.customerName')
+let expEnd = document.querySelector('.expEnd')
+let textCardNumber = document.querySelector('.textCardNumber')
+let textCustomerName = document.querySelector('.textCustomerName')
+let textExpEnd = document.querySelector('.textExpEnd')
+let button = document.querySelector('.buttonCardNumber')
+
+cardNumber.addEventListener('input', function () {
+    let cardNumberValue = cardNumber.value
+    textCardNumber.textContent = cardNumberValue
 })
-costumerName.addEventListener('input', function () {
-    let costumerNameValue = costumerName.value
-    textName.textContent = costumerNameValue
+customerName.addEventListener('input', function () {
+    let customerNameValue = customerName.value
+    textCustomerName.textContent = customerNameValue
 })
-iBAN.addEventListener('input', function () {
-    let iBANValue = iBAN.value
-    textIban.textContent = iBANValue
+expEnd.addEventListener('input', function () {
+    let expEndValue = expEnd.value
+    textExpEnd.textContent = expEndValue
 })
-bLZ.addEventListener('input', function () {
-    let bLZValue  = bLZ.value
-    textBlz.textContent = bLZValue
-})
-kontoNr.addEventListener('input', function () {
-    let kontoNrValue = kontoNr.value
-    textNr.textContent = kontoNrValue
-})
-date.addEventListener('input', function () {
-    let dateValue = date.value
-    textDate.textContent = dateValue
+button.addEventListener('click', function () {
+    let random = Math.floor(Math.random() * ( 9999 - 1000 ) + 1000)
+    let randomTwo = Math.floor(Math.random() * ( 9999 - 1000 ) + 1000)
+    let randomThree = Math.floor(Math.random() * ( 9999 - 1000 ) + 1000)
+    let randomFour = Math.floor(Math.random() * (9999 - 1000) + 1000)
+    let randomSum = `${random} ${randomTwo} ${randomThree} ${randomFour}`
+    textCardNumber.textContent = randomSum
 })
